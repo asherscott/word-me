7d1a5297464c09943148b286cba5f425956daad0
asher scott <asher.scott6@gmail.com>
2022-07-28 19:53:14 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [list, setList] = useState([]);
+  const [inputStr, setInputStr] = useState("");
+  // const [wordLength, setWordLength] = useState("");
+
+  function getComboNum(num, length) {
+    if (num < 3) return num;
+
+    let i = num;
+    let fac = 1;
+    while (i > num - (length || num)) {
+      fac *= i;
+      i--;
+    }
+
+    return fac;
+  }
+
+  function permutator(str) {
+    let result = [];
+
+    function permute(arr, m = []) {
+      if (arr.length === 0) {
+        result.push(m);
+      } else {
+        for (let i = 0; i < arr.length; i++) {
+          let curr = [...arr];
+          const next = curr.splice(i, 1);
+
+          permute([...curr], m.concat(next));
+        }
+      }
+    }
+
+    permute(str.split(""));
+
+    return result;
+  }
+
+  function scramble(string, wordLength) {
+    const comboNum = getComboNum(string.length, wordLength);
+
+    console.log(`Generating combos, ${comboNum} possible combinations...`);
+
+    setList(permutator(string));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    scramble(inputStr);
+  }
+
+  const renderList = list.map((word, idx) => <li key={idx}>{word}</li>);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => setInputStr(e.target.value)}
+          value={inputStr}
+          type="text"
+          placeholder="Enter String"
+          required
+        ></input>
+
+        {/* <input
+          onChange={(e) => setWordLength(e.target.value)}
+          value={wordLength}
+          type="number"
+          placeholder="optional"
+        ></input> */}
+        <button type="submit">enter</button>
+      </form>
+
+      <p>
+        {inputStr
+          ? getComboNum(inputStr.length) + " Possible Combinations"
+          : "Hey"}
+      </p>
+
+      <ul>{renderList}</ul>
     </div>
   );
 }
